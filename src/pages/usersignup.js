e561e136d6fb20e8f4f24a2400c90e8d5a07af78
Evan Teems <evanteems40@yahoo.com>
2021-05-17 21:24:44 -0700

    Login and Signup function Complete

---
+++ src/pages/usersignup.js
@@ -0,0 +1,25 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').valie.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({
+                usaername,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json'}
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
