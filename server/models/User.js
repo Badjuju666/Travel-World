7be7f054333390dbcd7795b036a1b523fd3757e6
Julian Graves <Juliangraves96@gmail.com>
2021-05-25 20:55:23 -0700

    bugsaway

--- server/models/User.js
+++ server/models/User.js
@@ -1,54 +1,51 @@
-const { Model, DataTypes } = require('mongoose');
-const mongoose = require('../config/connection.js');
+const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
-}
+const ticketSchema = require('./Ticket');
 
-User.init(
+const userSchema = new Schema(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
         username: {
-            type: DataTypes.STRING,
-            allowNull: true,
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
         },
         password: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            validate: {
-                len: [4]
-            }
-        }
-    },
-
-    {
-        hooks: {
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
+            type: String,
+            required: true,
         },
-        mongoose,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'User'
+        savedTickets: [ticketSchema],
     },
     {
-        mongoose
+        toJSON: {
+            virtuals: true,
+        },
     }
 );
+
+userSchema.pre('save', async function (next) {
+    if (this,isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('ticketsCount').get(function () {
+    return this.savedTickets.length;
+});
+
+const User = model('User', userSchema);
 
 module.exports = User;
--- server/models/index.js
+++ server/models/index.js
@@ -1,21 +1,21 @@
 const User = require('./User');
-const Tickets = require('./tickets');
+// const Ticket = require('./Ticket');
 const Destination = require('./Destination');
 
-Tickets.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// Ticket.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-Destination.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// Destination.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-Destination.HasMany(Tickets, {
-    foreignKey: 'destination_id',
-    onDelete: 'CASCADE'
-});
+// Destination.HasMany(Tickets, {
+//     foreignKey: 'destination_id',
+//     onDelete: 'CASCADE'
+// });
 
-module.exports = { User, Tickets, Destination}
+module.exports = { User, Destination}
 
